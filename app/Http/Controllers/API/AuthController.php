3c21b1fcf858ae5f37536eb88651608159ffd9cd
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-09-06 11:52:06 +0300

    AuthController & UserController

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(UserRequest $request): JsonResponse
+    {
+        $validatedData = $request->validated();
+        $validatedData['password'] = Hash::make($validatedData['password']);
+        $user = User::create($validatedData);
+
+        $token = $user->createToken('finwaveToken')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Registered successfully',
+            'token_type' => 'Bearer',
+            'token' => $token,
+        ]);
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Bad credentials, Try Again',
+            ], 422);
+        }
+
+        $token = $user->createToken('finwaveToken')->plainTextToken;
+        return response()->json([
+            'status' => 200,
+            'message' => 'Login Successfully',
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+    public function logout(): JsonResponse
+    {
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'Logout Successfully',
+        ], 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\CharityController;
 use App\Http\Controllers\API\ContactController;
 use App\Http\Controllers\API\GoalController;
 use App\Http\Controllers\API\IncomeController;
 use App\Http\Controllers\API\InvestmentController;
-use App\Http\Controllers\API\ProfileController;
+use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\PurchaseController;
 use App\Http\Controllers\API\ReportController;
 use App\Http\Controllers\API\StatisticController;
@@ -14,18 +15,16 @@ use App\Http\Controllers\API\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+//Protected Routes
 
-Route::apiResource('profiles', ProfileController::class)->except('index');
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::apiResource('users', UserController::class)->except('index');
 Route::apiResource('purchases',PurchaseController::class);
 Route::apiResource('investments',InvestmentController::class);
 Route::apiResource('incomes', IncomeController::class);
@@ -39,6 +38,4 @@ Route::get('statistics/{category}', [StatisticController::class, 'show']);
 Route::get('charities', [CharityController::class, 'index']);
 Route::post('contact', [ContactController::class, 'store']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
